64b4efec23e3f0fdbb0d44a480ab28d5b6fdf319
sphasuk <s.phasuk@lancaster.ac.uk>
2024-02-28 16:30:52 +0000

    nav bar + login stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sportsmate_flutter/pages/index.dart';
+import 'package:sportsmate_flutter/pages/login/loginpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Navigation(),
+      home: LoginPage(),
     );
   }
 }
--- lib/pages/login/loginpage.dart
+++ lib/pages/login/loginpage.dart
@@ -1,138 +1,165 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:sportsmate_flutter/components/text_field.dart';
 import 'package:sportsmate_flutter/pages/index.dart';
-import 'package:sportsmate_flutter/pages/login/sign_up.dart';
+import 'package:sportsmate_flutter/pages/sign_up.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
   final userNameController = TextEditingController();
   final passWordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 241, 250, 255),
-        body: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [
-                Color.fromARGB(255, 2, 135, 217),
-                Color.fromARGB(255, 255, 255, 255),
-              ],
-                  stops: [
-                0.0,
-                0.7
-              ],
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.topRight,
-                  transform: GradientRotation(0.5))),
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Column(
-                children: [
-                  //logo
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset('lib/images/sportmateLogo.png',
-                        height: 248),
-                  ),
-
-                  const SizedBox(height: 40),
-                  //text: Welcome to SportMate
-                  const Text(
-                    "Welcome to SportMate!",
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 0, 13, 70),
-                      fontSize: 20,
+      
+      backgroundColor: const Color.fromARGB(255, 241, 250, 255),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 2, 135, 217),
+              Color.fromARGB(255 , 255, 255, 255),
+            ],
+            stops: [0.0, 0.7],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            transform: GradientRotation(0.5)
+          )
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Form(
+              key: formKey,
+                child: Column(
+                  children: [
+                    //logo
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset('lib/images/sportmateLogo.png',
+                      height: 248),
                     ),
-                  ),
+                    //text: Welcome to SportMate
+                    const Text(
+                      "Welcome to SportMate!",
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                        
+                        color: Color.fromARGB(255, 0, 13, 70),
+                        
+                        fontSize: 20,
+                        
+                      ),
+                    ),
+                    
+                    const SizedBox(height: 80),
 
-                  const SizedBox(height: 80),
+                    TextFormField(
+                      validator: (value) {
+                        if(value!.isEmpty) {
+                          return "username is required";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        hintText: "Username"
+                      )
+                    ),
+                    const SizedBox(height: 25),
+                    TextFormField(
+                      validator: (value) {
+                        if(value!.isEmpty) {
+                          return "password is required";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        hintText: "Password"
+                      )
+                    ),
 
-                  textField(
-                    controller: userNameController,
-                    hintText: 'Username/Email',
-                  ),
-                  const SizedBox(height: 25),
-                  textField(
-                    controller: passWordController,
-                    hintText: 'Password',
-                  ),
+                    
 
-                  const SizedBox(height: 30),
+                    const SizedBox(height: 40),
 
-                  //login button
-                  GestureDetector(
-                    onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const Navigation(),
-                        )),
-                    child: Container(
+                    //login button
+                    GestureDetector(
+                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => Navigation()
+                      ,)
+                      
+                      ),
+                      child: Container(
                         decoration: BoxDecoration(
                           gradient: const LinearGradient(
-                              colors: [
-                                Color.fromARGB(255, 2, 133, 217),
-                                Color.fromARGB(255, 2, 38, 63),
-                              ],
-                              stops: [
-                                0.0,
-                                0.7
-                              ],
-                              //begin: Alignment.bottomLeft,
-                              //end: Alignment.topRight,
-                              transform: GradientRotation(0.5)),
-                          borderRadius: BorderRadius.circular(205),
+                            colors: [
+                              Color.fromARGB(255, 2, 133, 217),
+                              Color.fromARGB(255 , 2, 38, 63),
+                            ],
+                            stops: [0.0, 0.7],
+                            //begin: Alignment.bottomLeft,
+                            //end: Alignment.topRight,
+                            transform: GradientRotation(0.5)
+                          ),            
+                          borderRadius: BorderRadius.circular(205),      
                         ),
                         padding: const EdgeInsets.all(25),
                         child: const Center(
                           child: Text(
-                            "Log in",
+                            "Log in", 
                             style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'Poppins',
-                                fontSize: 20),
+                              color: Colors.white,
+                              fontFamily: 'Poppins',
+                              fontSize: 20
+                              ),
+                                  
                           ),
-                        )),
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  //text: Don't have an account
-                  const Text(
-                    "Don't have an account?",
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 0, 13, 70),
-                      fontSize: 13,
-                    ),
-                  ),
-
-                  //sign up button
-                  GestureDetector(
-                    onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SignUpPage(),
-                        )),
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        fontSize: 16.0,
-                        color: Color.fromARGB(255, 6, 41, 69),
+                        )
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
+
+                    const SizedBox(height: 20),
+                    
+                    //text: Don't have an account
+                    const Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                        
+                        color: Color.fromARGB(255, 0, 13, 70),
+                        
+                        fontSize: 13,
+                        
+                      ),
+                    ),
+
+                    //sign up button
+                    GestureDetector(
+                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()
+                      ,)
+                      
+                      ),
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 16.0,
+                          color: Color.fromARGB(255, 6, 41, 69),
+                        ),
+                      ),
+                    ),                   
+                  ],
+                ),
+
+            )
+            
           ),
-        ));
+        ),
+      )
+    );
   }
 }
