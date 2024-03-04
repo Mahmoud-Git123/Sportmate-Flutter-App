b7fcbd05593c64ababa3872c00b8f347a8513460
rowleyj5 <rowleyj5@lancaster.ac.uk>
2024-03-04 12:01:58 +0000

    added comments to helper, added all tables

--- lib/DbHelper.dart
+++ lib/DbHelper.dart
@@ -9,12 +9,13 @@ import 'package:sqflite/sqflite.dart';
 class DbHelper{
 
   static final _dbName = "sportMateDb";
-  static final _dbVersion = 1;
-  static final userTable = 'user';
-  
+  static final _dbVersion = 2;
+
+  // singleton class, one database across the app
   DbHelper._privateConstructor();
   static final DbHelper instance = DbHelper._privateConstructor();
 
+  // if database doesnt exist, then is created in the app's documents directory (_init and _onCreate methods are called)
   static Database? _sportMateDb;
   Future<Database> get database async {
     if (_sportMateDb != null) return _sportMateDb!;
@@ -22,6 +23,7 @@ class DbHelper{
     return _sportMateDb!;
   }
 
+  // initalises the database
   _init() async {
     WidgetsFlutterBinding.ensureInitialized();
     Directory docDir = await getApplicationDocumentsDirectory();
@@ -29,6 +31,7 @@ class DbHelper{
     return await openDatabase(dbPath, version: _dbVersion, onCreate: _onCreate);
   }
 
+  // creates each table
   _onCreate(Database db, int version) async {
     await db.execute('''
       CREATE TABLE user(
@@ -42,16 +45,61 @@ class DbHelper{
         elo FLOAT
       )
     ''');
+
+    await db.execute('''
+      CREATE TABLE availability(
+        id INTEGER PRIMARY KEY,
+        date TEXT,
+        time TEXT,
+        sport TEXT
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE match(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        sport TEXT,
+        date TEXT,
+        time TEXT,
+        location TEXT,
+        homeName TEXT,
+        awayName TEXT,
+        homeElo FLOAT,
+        homePredictedResult FLOAT,
+        awayElo FLOAT,
+        awayPredictedResult FLOAT,
+        gameResult TEXT
+      )
+    ''');
+
   }
 
-  Future<int> insertUser(Map<String, Object> user) async {
-    Database db = await instance.database;
-    return await db.insert(userTable, user);
-  }
+  // deletes the database (ONLY TO BE USED WHEN ADDING NEW TABLES OR CHANGING THE DATABASE STRUCTURE)
+  Future<void> deleteDatabase() async {
+    //gets path of database
+    Directory docDir = await getApplicationDocumentsDirectory();
+    String dbPath = join(docDir.path, _dbName);
+    bool exists = await File(dbPath).exists();
+    if (exists) {
+      // if the database file exists, delete it
+      await File(dbPath).delete();
+      print('Database deleted');
+    } else {
+      print('Database doesnt exist');
+    }
+}
 
-    Future<List<Map<String, dynamic>>> userGetAllRows() async {
+  
+  // insets into a table spceified by the params (name of table in '' and data in a map)
+  Future<int> insertToTable(String tableName, Map<String, Object> data) async {
     Database db = await instance.database;
-    return await db.query(userTable);
+    return await db.insert(tableName, data);
+  }
+  
+  // returns all rows from a table specified by the param (name of table in '')
+  Future<List<Map<String, dynamic>>> getAllRows(String tableName) async {
+    Database db = await instance.database;
+    return await db.query(tableName);
   }
 
 }
--- lib/DbTest.dart
+++ lib/DbTest.dart
@@ -30,7 +30,7 @@ class DbTest extends StatelessWidget {
                     'elo': 200,
                   };
 
-                await dbHelper.insertUser(testUser);
+                await dbHelper.insertToTable('user', testUser);
                 },
                 child: const Text('Insert test data'),
               ), // test insert 
@@ -38,16 +38,68 @@ class DbTest extends StatelessWidget {
               ElevatedButton(
                 onPressed: () async {
                   DbHelper dbHelper = DbHelper.instance;
-                  List<Map<String, dynamic>> userRows = await dbHelper.userGetAllRows();
+                  List<Map<String, dynamic>> userRows = await dbHelper.getAllRows('user');
                   print(userRows);
                 },
                 child: const Text('Get all rows'),
               ), // get rows
               ElevatedButton(
                 onPressed: () async {
+                  DbHelper dbHelper = DbHelper.instance;
+                  Map<String, Object> testAvailability = {
+                    'date': '12/04/2022',
+                    'time': '12:00',
+                    'sport': 'Tennis',
+                  };
+
+                  await dbHelper.insertToTable('availability', testAvailability);
                 },
-                child: const Text('Find user with specific email'),
+                child: const Text('Availibility test'), 
               ), // query
+              ElevatedButton(
+                onPressed: () async {
+                  DbHelper dbHelper = DbHelper.instance;
+                  List<Map<String, dynamic>> availabilityRows = await dbHelper.getAllRows('availability');
+                  print(availabilityRows);
+                },
+                child: const Text('Get all availibility rows'),
+              ), // get rows
+              ElevatedButton(
+                onPressed: () async {
+                  DbHelper dbHelper = DbHelper.instance;
+                  Map<String, Object> testMatch = {
+                    'sport': 'Tennis',
+                    'date': '12/04/2022',
+                    'time': '12:00',
+                    'location': 'London',
+                    'homeName': 'Emily',
+                    'awayName': 'John',
+                    'homeElo': 200,
+                    'homePredictedResult': 0.5,
+                    'awayElo': 200,
+                    'awayPredictedResult': 0.5,
+                    'gameResult': 'draw',
+                  };
+
+                  await dbHelper.insertToTable('match', testMatch);
+                },
+                child: const Text('Match test'), 
+              ), // query
+              ElevatedButton(
+                onPressed: () async {
+                  DbHelper dbHelper = DbHelper.instance;
+                  List<Map<String, dynamic>> matchRows = await dbHelper.getAllRows('match');
+                  print(matchRows);
+                },
+                child: const Text('Get all match rows'),
+              ), // get rows
+              ElevatedButton(
+                onPressed: () async {
+                  DbHelper dbHelper = DbHelper.instance;
+                  await dbHelper.deleteDatabase();
+                },
+                child: const Text('Delete database'),
+              ), // delete database
             ]
           ),
         ),
