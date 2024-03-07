ce11a9e228b8237022661ee2d22c88b1c5c3fa3a
rowleyj5 <rowleyj5@lancaster.ac.uk>
2024-03-07 20:36:20 +0000

    added a global version of username, check readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sportsmate_flutter/DbTest.dart';
 import 'package:sportsmate_flutter/pages/index.dart';
 import 'package:sportsmate_flutter/pages/login/loginpage.dart';
+import 'userName.dart'; // Import the UsernameProvider class
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(SportMate());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => UsernameProvider(),
+      child: SportMate(),
+    ),
+  );
 }
 
 class SportMate extends StatelessWidget {
-  const SportMate({super.key});
+  const SportMate({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
