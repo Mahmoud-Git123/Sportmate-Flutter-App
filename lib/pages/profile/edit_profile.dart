19ea8baf6a0dc39cb786580d327143333c17cba2
sphasuk <s.phasuk@lancaster.ac.uk>
2024-03-12 20:07:37 +0000

    profile and avatar

--- lib/pages/profile/edit_profile.dart
+++ lib/pages/profile/edit_profile.dart
@@ -22,47 +22,62 @@ class EditProfileScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const SizedBox(
-              width: 120,
-              height: 120,
-              child: CircleAvatar(
-                radius: 25,
-                backgroundColor: Color.fromARGB(255, 230, 245, 254),
-                child: Icon(Icons.person,
-                    size: 60, color: Color.fromARGB(255, 143, 72, 72)),
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Divider(),
-            const SizedBox(height: 10),
-            _buildFormField(Icons.person, "First Name"),
-            const SizedBox(height: 10),
-            _buildFormField(Icons.person, "Last Name"),
-            const SizedBox(height: 10),
-            _buildFormField(Icons.email, "Email"),
-            const SizedBox(height: 10),
-            _buildFormField(Icons.person_outline, "Username"),
-            const SizedBox(height: 10),
-            _buildFormField(Icons.lock, "Password", obscureText: true),
-            const SizedBox(height: 10),
-            _buildFormField(Icons.lock, "Confirm Password", obscureText: true),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                // Implement save functionality here
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.lightBlue, // background color
-                  side: BorderSide.none,
-                  minimumSize: const Size(200, 50),
-                  shape: const StadiumBorder()),
-              child: const Text('Save'),
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+            Color.fromARGB(255, 2, 135, 217),
+            Color.fromARGB(255, 255, 255, 255),
           ],
+              stops: [
+            0.0,
+            0.7
+          ],
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              transform: GradientRotation(0.5))),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                width: 120,
+                height: 120,
+                child: CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Color.fromARGB(255, 230, 245, 254),
+                  child: Icon(Icons.person,
+                      size: 60, color: Color.fromARGB(255, 143, 72, 72)),
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Divider(),
+              const SizedBox(height: 10),
+              _buildFormField(Icons.person, "First Name"),
+              const SizedBox(height: 10),
+              _buildFormField(Icons.person, "Last Name"),
+              const SizedBox(height: 10),
+              _buildFormField(Icons.email, "Email"),
+              const SizedBox(height: 10),
+              _buildFormField(Icons.person_outline, "Username"),
+              const SizedBox(height: 10),
+              _buildFormField(Icons.lock, "Password", obscureText: true),
+              const SizedBox(height: 10),
+              _buildFormField(Icons.lock, "Confirm Password", obscureText: true),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlue, // background color
+                    side: BorderSide.none,
+                    minimumSize: const Size(200, 50),
+                    shape: const StadiumBorder()),
+                child: const Text('Save'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/profile/my_profile.dart
+++ lib/pages/profile/my_profile.dart
@@ -13,7 +13,9 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final username = Provider.of<UsernameProvider>(context, listen: false).username;
     String email = Provider.of<UsernameProvider>(context).email;
+
     DbHelper dbHelper = DbHelper.instance;
+
     return Scaffold(
       appBar: AppBar(
         title: const Padding(
@@ -31,132 +33,147 @@ class ProfileScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // user information section
-            const SizedBox(
-              width: 120,
-              height: 120,
-              child: CircleAvatar(
-                radius: 25,
-                backgroundColor: Color.fromARGB(255, 230, 245, 254),
-                child: Icon(Icons.person, size: 25, color: Colors.grey),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(username),
-            Text(email),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () {
-                  // implement Edit profile logic here
-                  // should open new page that allows user to edit account details
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const EditProfileScreen()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 230, 245, 254),
-                    side: BorderSide.none,
-                    minimumSize: const Size(200, 50),
-                    shape: const StadiumBorder()),
-                child: const Text('Edit Profile',
-                    style: TextStyle(color: Colors.black)),
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Divider(),
-            const SizedBox(height: 10),
-
-            // MENU
-            Column(
-              children: [
-                _buildMenuButton(
-                    text: 'Your Schedule',
-                    icon: Icons.timelapse,
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SchedulePage(),
-                        ),
-                      );
-                    }),
-                _buildMenuButton(
-                  text: 'Sports Subscriptions',
-                  icon: Icons.sports_soccer,
-                  onPressed: () {},
-                ),
-                _buildMenuButton(
-                    text: 'Settings', icon: Icons.settings, onPressed: () {}),
-                const Divider(),
-                _buildMenuButton(
-                    text: 'Information',
-                    icon: Icons.info,
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text("Credits"),
-                              content: const Text(
-                                  "Joshua Rowley\nSye Phasuk\nRhys Woods\nKuwsh Okai\nJojo Andoh\nMahmoud Abdelfattah\nStanislaw Nowaczyk"),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text('Close'),
-                                ),
-                              ],
-                            );
-                          });
-                    }),
-                _buildMenuButton(
-                    text: 'Logout',
-                    icon: Icons.logout,
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text("Logout"),
-                              content: const Text(
-                                  "Are you sure you want to logout?"),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text('Cancel'),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    // logic for logging out the user goes here
-                                    Provider.of<UsernameProvider>(context, listen: false).reset();
-                                    Navigator.of(context).pop(); // Close the dialog
-                                                    Navigator.pushReplacement(
-                                                      context,
-                                                      MaterialPageRoute(
-                                                        builder: (context) => LoginPage(),
-                                                      ),
-                                                    );
-                                  },
-                                  child: const Text('Logout'),
-                                )
-                              ],
-                            );
-                          });
-                    }),
-              ],
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+            Color.fromARGB(255, 2, 135, 217),
+            Color.fromARGB(255, 255, 255, 255),
           ],
+              stops: [
+            0.0,
+            0.7
+          ],
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              transform: GradientRotation(0.5))),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // user information section
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Color.fromARGB(255, 230, 245, 254),
+                  child: Image.asset('lib/images/sportmateLogo.png'),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(username),
+              Text(email),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // implement Edit profile logic here
+                    // should open new page that allows user to edit account details
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const EditProfileScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 230, 245, 254),
+                      side: BorderSide.none,
+                      minimumSize: const Size(200, 50),
+                      shape: const StadiumBorder()),
+                  child: const Text('Edit Profile',
+                      style: TextStyle(color: Colors.black)),
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Divider(),
+              const SizedBox(height: 10),
+        
+              // MENU
+              Column(
+                children: [
+                  _buildMenuButton(
+                      text: 'Your Schedule',
+                      icon: Icons.timelapse,
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SchedulePage(),
+                          ),
+                        );
+                      }),
+                  _buildMenuButton(
+                    text: 'Sports Subscriptions',
+                    icon: Icons.sports_soccer,
+                    onPressed: () {},
+                  ),
+                  _buildMenuButton(
+                      text: 'Settings', icon: Icons.settings, onPressed: () {}),
+                  const Divider(),
+                  _buildMenuButton(
+                      text: 'Information',
+                      icon: Icons.info,
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("Credits"),
+                                content: const Text(
+                                    "Joshua Rowley\nSye Phasuk\nRhys Woods\nKuwsh Okai\nJojo Andoh\nMahmoud Abdelfattah\nStanislaw Nowaczyk"),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('Close'),
+                                  ),
+                                ],
+                              );
+                            });
+                      }),
+                  _buildMenuButton(
+                      text: 'Logout',
+                      icon: Icons.logout,
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("Logout"),
+                                content: const Text(
+                                    "Are you sure you want to logout?"),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      // logic for logging out the user goes here
+                                      Provider.of<UsernameProvider>(context, listen: false).reset();
+                                      Navigator.of(context).pop(); // Close the dialog
+                                                      Navigator.pushReplacement(
+                                                        context,
+                                                        MaterialPageRoute(
+                                                          builder: (context) => LoginPage(),
+                                                        ),
+                                                      );
+                                    },
+                                    child: const Text('Logout'),
+                                  )
+                                ],
+                              );
+                            });
+                      }),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -171,6 +188,7 @@ class ProfileScreen extends StatelessWidget {
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromARGB(255, 204, 221, 235),
           side: BorderSide.none,
           shape: const StadiumBorder(),
           minimumSize: const Size(200, 50),
