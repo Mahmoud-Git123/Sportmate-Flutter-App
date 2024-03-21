67af4e82b248a62a2e1768a38a5e82ee640e7edd
stasnowaczyk <stas.nowaczyk@gmail.com>
2024-03-21 15:31:34 +0100

    Added schedule test and fixed profile test

--- test/ui_tests/profile_page_test.dart
+++ test/ui_tests/profile_page_test.dart
@@ -1,17 +1,24 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:sportsmate_flutter/pages/profile/my_profile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 import 'package:sportsmate_flutter/pages/profile/index.dart';
+import 'package:sportsmate_flutter/pages/profile/my_profile.dart';
+import 'package:sportsmate_flutter/userName.dart';
 
 void main() {
   testWidgets('Profile widget should be created and rendered correctly',
       (WidgetTester tester) async {
-    // Build SporMate app and trigger a frame
-    await tester.pumpWidget(const Profile());
+    // Build SportMate app and trigger a frame
+    await tester.pumpWidget(
+      ChangeNotifierProvider<UsernameProvider>(
+        create: (_) => UsernameProvider(),
+        child: const Profile(),
+      ),
+    );
 
     // Ensure that the Profile page is rendered
     expect(find.byType(Profile), findsOneWidget);
     expect(find.byType(ProfileScreen), findsOneWidget);
     expect(find.byType(MaterialApp), findsOneWidget);
   });
-}
+} 
